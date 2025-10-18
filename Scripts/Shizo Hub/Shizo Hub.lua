3fb85dd30285dd2a950da9b0948602f74f1c28a4
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-18 23:38:58 +0800

    Update Shizo Hub.lua

--- Scripts/Shizo Hub/Shizo Hub.lua
+++ Scripts/Shizo Hub/Shizo Hub.lua
@@ -1,3 +1,12 @@
+local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Script Made By",
+    Text = "Shizoscript",
+    Icon = "rbxassetid://29819383",
+    Duration = 8,
+})
+
 -- Load the library
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vxvV4/Roblox/refs/heads/main/Scripts/Shizo%20Hub/Shizo%20Hub%20Library.lua"))()
 
@@ -7,8 +16,7 @@ Library:SetTitle("••• Shizo ••• Hub •••")
 -- Create tabs
 local mainTab = Library:CreateTab("Main")
 
--- Add buttons to Main tab
-mainTab:AddButton("Infinite Jump", function()
+-- // *** Add button Main Tab *** \\ --
+mainTab:AddButton("Walk Speed", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vxvV4/Roblox/refs/heads/main/Scripts/Settings/Walk%20speed.lua"))()
-    end)
 end)
