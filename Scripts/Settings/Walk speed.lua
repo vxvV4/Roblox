eaac8563a861b093eae97d2a54ee4dff0077a6fd
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-18 23:30:25 +0800

    Update Walk speed.lua

--- Scripts/Settings/Walk speed.lua
+++ Scripts/Settings/Walk speed.lua
@@ -1,3 +1,12 @@
+local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Script Made By",
+    Text = "Shizoscript",
+    Icon = "rbxassetid://29819383",
+    Duration = 2,
+})
+
 local SliderLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vxvV4/Roblox/refs/heads/main/Scripts/Shizo%20Hub/Slider%20Gui%20Shizo/Source.lua"))()
 
 SliderLib:CreateSlider("Walk Speed", 16, 200, 16, function(value)
