667a12c3144d014d5fa6ee36057bc3817331ac8f
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-18 23:02:10 +0800

    Create Example.lua

---
+++ Scripts/Shizo Hub/Button Gui Shizo/Example.lua
@@ -0,0 +1,5 @@
+local ButtonLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vxvV4/Roblox/refs/heads/main/Scripts/Shizo%20Hub/Button%20Gui%20Shizo/Source.lua"))()
+
+ButtonLib:CreateButton("Kill All", function()
+    print("Kill All clicked!")
+end)
