627763f56f0e409a7cd471fa94a9c2722bb116c7
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-20 14:21:32 +0800

    Update YouTube Examle.lua

--- Script Maker/Useful/YouTube Examle.lua
+++ Script Maker/Useful/YouTube Examle.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("http://vpaste.net/4NYC0"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vxvV4/Roblox/refs/heads/main/Script%20Maker/Useful/YouTube%20Library.lua"))()
 
 local Window = Library:CreateWindow("Be a Beggar!")
 
